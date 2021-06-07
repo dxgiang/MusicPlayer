72c3c7ca0c48785047789432e871a67ee5d1a44f
DinhXuanGiang <80635552+DinhXuanGiang@users.noreply.github.com>
2021-06-07 20:33:07 +0700

    done AlbumCategory

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,19 +1,42 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Album from '../components/Album';
+import AlbumCategory from '../components/AlbumCategory';
 
-const album = {
+const albumCategory = {
   id: '1',
-  imageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzC2JyZDZ_RaWf0qp11K0lcvB6b6kYNMoqtZAQ9hiPZ4cTIOB',
-  artistsHeadline: 'Taylor Swift, Avicii'
+  title: 'Google',
+  albums: [
+    {
+      id: '1',
+      imageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzC2JyZDZ_RaWf0qp11K0lcvB6b6kYNMoqtZAQ9hiPZ4cTIOB',
+      artistsHeadline: 'Taylor Swift, Avicii'
+    },
+    {
+      id: '2',
+      imageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzC2JyZDZ_RaWf0qp11K0lcvB6b6kYNMoqtZAQ9hiPZ4cTIOB',
+      artistsHeadline: 'Post Malone, Avicii'
+    },
+    {
+      id: '3',
+      imageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzC2JyZDZ_RaWf0qp11K0lcvB6b6kYNMoqtZAQ9hiPZ4cTIOB',
+      artistsHeadline: 'Journey, Avicii'
+    },
+    {
+      id: '4',
+      imageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzC2JyZDZ_RaWf0qp11K0lcvB6b6kYNMoqtZAQ9hiPZ4cTIOB',
+      artistsHeadline: 'Escape, Avicii'
+    },
+  ]
 }
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Album album={album}>
-      </Album>
+      <AlbumCategory 
+        title={albumCategory.title} 
+        albums={albumCategory.albums}
+      />
     </View>
   );
 }
