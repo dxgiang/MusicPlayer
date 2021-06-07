3e6b35a53e52bfe7b94409f18578b9ef4b9ed2ba
DinhXuanGiang <80635552+DinhXuanGiang@users.noreply.github.com>
2021-06-07 21:26:48 +0700

    create albumscreen.tsx

---
+++ screens/AlbumScreen.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+const AlbumScreen = () => (
+    <View>
+        <Text style={{color: 'white'}}>This is Album Screnn</Text>
+    </View>
+)
+
+export default AlbumScreen;
