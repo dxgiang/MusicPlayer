10f7a43b93b56df22846e3d79eb3b2c069cca6d5
DinhXuanGiang <80635552+DinhXuanGiang@users.noreply.github.com>
2021-06-07 22:15:08 +0700

    minor change

---
+++ components/SongListItem/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, Image, View } from "react-native";
+import styles from './styles';
+
+export type SongListItemProps = {
+    song
+}
+
+const SongListItem = (props: SongListItemProps) => {
+
+}
