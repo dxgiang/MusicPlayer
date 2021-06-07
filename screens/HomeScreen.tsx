719dce963af69750c5fab85630b8a15b4a36a520
DinhXuanGiang <80635552+DinhXuanGiang@users.noreply.github.com>
2021-06-07 21:08:03 +0700

    minor change

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,41 +1,21 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, FlatList, View } from 'react-native';
 
 import AlbumCategory from '../components/AlbumCategory';
+import albumCategories from '../data/albumCategories';
 
-const albumCategory = {
-  id: '1',
-  title: 'Google',
-  albums: [
-    {
-      id: '1',
-      imageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzC2JyZDZ_RaWf0qp11K0lcvB6b6kYNMoqtZAQ9hiPZ4cTIOB',
-      artistsHeadline: 'Taylor Swift, Avicii'
-    },
-    {
-      id: '2',
-      imageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzC2JyZDZ_RaWf0qp11K0lcvB6b6kYNMoqtZAQ9hiPZ4cTIOB',
-      artistsHeadline: 'Post Malone, Avicii'
-    },
-    {
-      id: '3',
-      imageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzC2JyZDZ_RaWf0qp11K0lcvB6b6kYNMoqtZAQ9hiPZ4cTIOB',
-      artistsHeadline: 'Journey, Avicii'
-    },
-    {
-      id: '4',
-      imageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzC2JyZDZ_RaWf0qp11K0lcvB6b6kYNMoqtZAQ9hiPZ4cTIOB',
-      artistsHeadline: 'Escape, Avicii'
-    },
-  ]
-}
-
-export default function TabOneScreen() {
+export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <AlbumCategory 
-        title={albumCategory.title} 
-        albums={albumCategory.albums}
+      <FlatList 
+        data={albumCategories}
+        renderItem={({ item }) => (
+          <AlbumCategory 
+            title={item.title}
+            albums={item.albums}
+            keyExtractor={(item) => item.id}
+          />
+        )}
       />
     </View>
   );
