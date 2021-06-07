4f4f40e9bc8a2642c50836b077996a7ab0bc84ec
DinhXuanGiang <80635552+DinhXuanGiang@users.noreply.github.com>
2021-06-07 18:18:33 +0700

    update homescreen.tsx

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Album from '../components/Album';
+
+const album = {
+  id: '1',
+  imageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzC2JyZDZ_RaWf0qp11K0lcvB6b6kYNMoqtZAQ9hiPZ4cTIOB',
+  artistsHeadline: 'Taylor Swift, Avicii'
+}
+
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={{color: 'white'}}>Music</Text>
+      <Album album={album}>
+      </Album>
     </View>
   );
 }
