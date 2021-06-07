3c111a845de31f7d3c3a84d9c177791e2e33cc63
DinhXuanGiang <80635552+DinhXuanGiang@users.noreply.github.com>
2021-06-07 19:00:58 +0700

    minor fix

--- components/Album/index.tsx
+++ components/Album/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, Image, Text } from "react-native";
+import React from 'react';
+import { View, Image, Text } from 'react-native';
 import styles from './styles';
 
 export type AlbumProps = {
