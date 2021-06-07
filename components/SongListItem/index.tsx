cc49c465baff543cc3d401de8435a43df083e224
DinhXuanGiang <80635552+DinhXuanGiang@users.noreply.github.com>
2021-06-07 22:54:00 +0700

    done AlbumScreen - SongList

--- components/SongListItem/index.tsx
+++ components/SongListItem/index.tsx
@@ -11,10 +11,12 @@ const SongListItem = (props: SongListItemProps) => {
     const {song} = props;
 
     return (
-        <View>
-            <Image source={{ uri: props.song.imageUri }} style={styles.image} />
-            <Text style={styles.title}>{song.title}</Text>
-            <Text style={styles.artist}>{song.artist}</Text>
+        <View style={styles.container}>
+            <Image source={{ uri: song.imageUri }} style={styles.image} />
+            <View style={styles.rightContainer}>
+                <Text style={styles.title}>{song.title}</Text>
+                <Text style={styles.artist}>{song.artist}</Text>
+            </View>
         </View>
     )
 
--- screens/AlbumScreen.tsx
+++ screens/AlbumScreen.tsx
@@ -1,46 +1,24 @@
 import { useRoute } from '@react-navigation/native';
 import * as React from 'react';
-import { Text, View } from 'react-native';
-
-const album = {
-    id: '8',
-    name: 'Good vibes',
-    by: 'Spotify',
-    numberOfLikes: 38,
-    imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
-    artistsHeadline: 'Taylor Swift, Kygo Objective C, Avicii',
-    song: [{
-        id: '1',
-        imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
-        title: 'High on You',
-        artist: 'Helen',
-    },
-    {
-        id: '2',
-        imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
-        title: 'Neon Pursuit',
-        artist: 'Helen',
-    },
-    {
-        id: '3',
-        imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
-        title: 'Back to You',
-        artist: 'Helen',
-    },
-    {
-        id: '4',
-        imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
-        title: 'What ever yo want to be',
-        artist: 'Helen',
-    }]
-  }
+import { Text, View , FlatList } from 'react-native';
+import albumDetails from '../data/albumDetails';
+import SongListItem from '../components/SongListItem';
+import { useEffect } from 'react';
 
 const AlbumScreen = () => {
     const route = useRoute();
 
+    useEffect( () => {
+        console.log(route);
+    })
+
     return (
         <View>
-            <Text style={{color: 'white'}}>This is Album Screnn</Text>
+            <FlatList
+                data={albumDetails.songs}
+                renderItem={({ item }) => <SongListItem song={item} />}
+                keyExtractor={( item ) => item.id}
+            />
         </View>
     )
 }
